de2d2f1c9eb1ecceecb50a24911830a339f1b826
GustavoAntony <gustavoa9@al.insper.edu.br>
2024-04-10 11:08:51 -0300

    adicionando camera seguindo player

--- ChaseQuest/Assets/Scripts/PlayerController.cs
+++ ChaseQuest/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    private Rigidbody2D _playerRb;
+    public float _playerSpeed;
+    private Vector2 _playerDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {   
+        _playerRb.MovePosition(_playerRb.position + _playerDirection * _playerSpeed * Time.fixedDeltaTime);
     }
 }
